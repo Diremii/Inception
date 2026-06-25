daa7af0dde5491bd69a386fcbe34b0d311e29e99
Hugo M. <144733665+Diremii@users.noreply.github.com>
2026-06-25 16:13:57 +0200

    Add files via upload

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
-sleep 10
-
-until mysqladmin ping -h mariadb -u${SQL_USER} -p${SQL_PASSWORD} --silent; do
-    echo "Waiting for MariaDB..."
-    sleep 2
-done
-
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
+    echo "Setting up Wp"
     wp config create --allow-root \
         --dbname=$SQL_DATABASE \
         --dbuser=$SQL_USER \
         --dbpass=$SQL_PASSWORD \
-        --dbhost=mariadb:3306 \
+        --dbhost=mariadb \
         --path=/var/www/wordpress
+
+    wp core install --allow-root \
+        --url=$DOMAIN_NAME \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --path=/var/www/wordpress
+
+    wp user create $WP_USER $WP_USER_EMAIL \
+        --user_pass=$WP_USER_PASSWORD \
+        --role=subscriber \
+        --path=/var/www/wordpress || true
 fi
 
-wp core install --allow-root \
-    --url=$DOMAIN_NAME \
-    --title=$WP_TITLE \
-    --admin_user=$WP_ADMIN \
-    --admin_password=$WP_ADMIN_PASSWORD \
-    --admin_email=$WP_ADMIN_EMAIL \
-    --path=/var/www/wordpress
-
-wp user create $WP_USER $WP_USER_EMAIL \
-    --user_pass=$WP_USER_PASSWORD \
-    --role=subscriber \
-    --path=/var/www/wordpress || true
 
 exec php-fpm8.2 -F
